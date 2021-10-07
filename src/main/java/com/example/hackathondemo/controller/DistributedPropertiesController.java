0206837e076ff698a7df6d55a4f8bffb097622c8
Patricia J. Carranza <pcarranza@backblaze.com>
2021-10-07 15:18:03 -0700

    Rename app

--- src/main/java/com/example/hackathondemo/controller/DistributedPropertiesController.java
+++ src/main/java/com/example/hackathondemo/controller/DistributedPropertiesController.java
@@ -14,16 +14,12 @@ import java.util.Map;
 @CrossOrigin(origins = { "http://localhost:3000" })
 public class DistributedPropertiesController {
 
-    @Value("${props.test}")
-    String value;
-
     @Value("${props.renderFilesButton}")
     boolean renderFilesButton;
 
     @GetMapping("/getConfigFromValue")
     public Map<String, Object> getConfigFromValue() {
         Map<String, Object> configMap = new HashMap<>();
-        configMap.put("test", value);
         configMap.put("renderFilesButton", renderFilesButton);
         return configMap;
     }
