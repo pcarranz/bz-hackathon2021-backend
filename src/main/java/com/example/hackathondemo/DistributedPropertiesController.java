97565b880fa2647d509e65b3aca337e9281035bc
Patricia J. Carranza <pcarranza@backblaze.com>
2021-10-07 14:30:25 -0700

    Update properties endpoint to return map

--- src/main/java/com/example/hackathondemo/DistributedPropertiesController.java
+++ src/main/java/com/example/hackathondemo/DistributedPropertiesController.java
@@ -4,22 +4,33 @@ import com.example.hackathondemo.entity.Switches;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RefreshScope
+@CrossOrigin(origins = { "http://localhost:3000" })
 public class DistributedPropertiesController {
 
     @Value("${props.test}")
     String value;
 
+    @Value("${props.renderFilesButton}")
+    boolean renderFilesButton;
+
     @Autowired
     private Switches properties;
 
     @GetMapping("/getConfigFromValue")
-    public String getConfigFromValue() {
-        return value;
+    public Map<String, Object> getConfigFromValue() {
+        Map<String, Object> configMap = new HashMap<>();
+        configMap.put("test", value);
+        configMap.put("renderFilesButton", renderFilesButton);
+        return configMap;
     }
 
     @GetMapping("/getConfigFromProperty")
